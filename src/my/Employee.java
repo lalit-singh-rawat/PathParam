976b1de7d75f2307074e91743e76d18e75b689bd
lalit123 <rawatsinghlalit@gmail.com>
2021-06-10 15:11:30 +0530

    "Initial commit by Lalit"

---
+++ src/my/Employee.java
@@ -0,0 +1,15 @@
+package my;
+
+public class Employee{
+ //File created and added directly in GitHub for testing purpose
+	public static void main() {
+		int[] numbers = { 125, 132, 95, 116, 110 }; 
+		// for each loop
+        for (int num : numbers)
+        {
+            System.out.println("number is "+num);
+        }
+			
+		
+	}
+}
